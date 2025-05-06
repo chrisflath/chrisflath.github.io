0b34d99273598b9b1ad8bededf7c118dbe6e6e07
CMF <christoph.flath@uni-wuerzburg.de>
2025-05-06 20:22:42 +0200

    path fix

--- notebooks/sql-murder.py
+++ notebooks/sql-murder.py
@@ -14,7 +14,7 @@ def _():
 
 
     # URL of the dataset (must allow direct downloads)
-    url = "https://raw.githubusercontent.com//NUKnightLab/sql-mysteries/raw/refs/heads/master/sql-murder-mystery.db"
+    url = "https://raw.githubusercontent.com/WIBA-DMA/exam-2024-pprm/refs/heads/main/sql-murder-mystery.db"
 
     # Download and save the file
     filename = "murder.db"
