26117c70332a84c5a6638e6a3e1e147bcbf41746
chrisflath <christoph.flath@uni-wuerzburg.de>
2025-07-09 09:22:36 +0200

    Add files via upload

--- notebooks/Zeitreihenanalyse-GPUs.py
+++ notebooks/Zeitreihenanalyse-GPUs.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.14.9"
+__generated_with = "0.14.10"
 app = marimo.App(width="full")
 
 
@@ -34,7 +34,7 @@ def _(requests, sqlalchemy):
     return (engine,)
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""## Data Science Salaries""")
     return
@@ -51,7 +51,7 @@ def _(ds_salaries, engine, mo):
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
@@ -64,31 +64,124 @@ def _(mo):
 
 
 @app.cell
+def _(ds_salaries, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT
+            company_location,
+            ROUND(AVG(salary_in_usd)) AS average_salary,
+            COUNT(*) AS numberOfOffers
+        FROM
+            ds_salaries
+        GROUP BY
+            company_location
+        HAVING
+            numberOfOffers >= 5
+        ORDER BY
+            average_salary desc
+        LIMIT
+            10
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""- In welchen Ländern arbeiten besonders viele Mitarbeiter im Homeoffice? In welchen besonders wenige?""")
     return
 
 
 @app.cell
+def _(ds_salaries, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT
+            company_location,
+            AVG(remote_ratio) AS avg_remote,
+            COUNT(*) AS numberOfOffers
+        FROM
+            ds_salaries
+        GROUP BY
+            company_location
+        HAVING
+            numberOfOffers >= 5
+        ORDER BY
+            avg_remote desc
+        LIMIT
+            10
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
     Der bereitgestellte Datensatz enthält Meldungen aus mehreren Jahren.
 
     - Wie haben sich die gemeldeten Gehälter im Bereich Data Science in den letzten Jahren entwickelt?
-
     """
     )
     return
 
 
 @app.cell
+def _(ds_salaries, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT
+            company_location,
+            work_year,
+            ROUND(AVG(salary_in_usd)) AS average_salary
+        FROM
+            ds_salaries
+        WHERE
+            company_location IN ('US', 'DE', 'GB')
+        GROUP BY
+            work_year,
+            company_location
+        ORDER BY
+            company_location,
+            work_year
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""## GPU Preise""")
     return
 
 
 @app.cell
+def _(GPU, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM GPU
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(Prices, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM Prices
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
@@ -101,6 +194,29 @@ def _(mo):
 
 
 @app.cell
+def _(engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT  GPU_ID, MIN(Price), AVG(Price), MAX(Price) FROM `Prices`
+        GROUP BY GPU_ID
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(GPU, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM GPU WHERE ID = '2167257'
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
@@ -112,15 +228,188 @@ def _(mo):
 
 
 @app.cell
+def _(Prices, engine, mo):
+    temp = mo.sql(
+        f"""
+        SELECT
+            DISTINCT(GPU_ID),
+            FIRST_VALUE(Price) OVER (PARTITION BY GPU_ID ORDER BY Date) AS first_price,
+            julianday(FIRST_VALUE(Date) OVER (PARTITION BY GPU_ID ORDER BY Date)) AS first_date,
+            LAST_VALUE(Price) OVER (PARTITION BY GPU_ID ORDER BY Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_price,
+            julianday(LAST_VALUE(Date) OVER (PARTITION BY GPU_ID ORDER BY Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) AS last_date
+        FROM
+            Prices
+        GROUP BY
+            GPU_ID, Price -- Include all non-aggregated columns in GROUP BY
+        ORDER BY
+            GPU_ID, Date;
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(Prices, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT gpu_id, (last_price - first_price) / (last_date - first_date) as slope FROM
+            (SELECT
+            DISTINCT(GPU_ID),
+            FIRST_VALUE(Price) OVER (PARTITION BY GPU_ID ORDER BY Date) AS first_price,
+            julianday(FIRST_VALUE(Date) OVER (PARTITION BY GPU_ID ORDER BY Date)) AS first_date,
+            LAST_VALUE(Price) OVER (PARTITION BY GPU_ID ORDER BY Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS last_price,
+            julianday(LAST_VALUE(Date) OVER (PARTITION BY GPU_ID ORDER BY Date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)) AS last_date
+        FROM
+            Prices
+        GROUP BY
+            GPU_ID, Price -- Include all non-aggregated columns in GROUP BY
+        ORDER BY
+            GPU_ID, Date)
+        ORDER BY slope
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell(hide_code=True)
 def _(mo):
     mo.md(
         r"""
     Ihr Kollege behauptet, dass GPU-Preise saisonalen Schwankungen (Weihnachten, Ferienzeit) unterliegen.
 
     - Aggregieren Sie alle Karten mit dem RTX 3080 Ti Chip und bestimmen Sie die saisonalen Faktoren um diese These zu untersuchen. Denken Sie an die voherige Trendbereinigung!
-
     """
     )
+    return
+
+
+@app.cell
+def _(GPU, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT Name, ID FROM GPU
+        WHERE Name LIKE "%3090%"
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(Prices, engine, mo):
+    _df = mo.sql(
+        f"""
+        SELECT
+          strftime('%Y', Date)    AS sales_year,
+          strftime('%m', Date)    AS sales_month,
+          AVG(Price)              AS avgPrice,
+          julianday(MIN(Date))    AS month_jd
+        FROM Prices
+        WHERE GPU_ID IN ('2364388','2366077','2366719')
+        GROUP BY sales_year, sales_month
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(Prices, engine, mo):
+    _df = mo.sql(
+        f"""
+        -- 1) Create a temp table holding your trend parameters
+        CREATE TEMP TABLE temp_trend AS
+        SELECT
+          FIRST_VALUE(avgPrice) OVER w    AS first_price,
+          LAST_VALUE (avgPrice) OVER w    AS last_price,
+          FIRST_VALUE(month_jd)  OVER w   AS first_julianday,
+          LAST_VALUE (month_jd)  OVER w   AS last_julianday,
+          (
+            LAST_VALUE (avgPrice) OVER w
+            - FIRST_VALUE(avgPrice) OVER w
+          )
+          /
+          (
+            LAST_VALUE (month_jd)  OVER w
+            - FIRST_VALUE(month_jd)  OVER w
+          )                                AS slope
+        FROM (
+          SELECT
+            strftime('%Y', Date)    AS sales_year,
+            strftime('%m', Date)    AS sales_month,
+            AVG(Price)              AS avgPrice,
+            julianday(MIN(Date))    AS month_jd
+          FROM Prices
+          WHERE GPU_ID IN ('2364388','2366077','2366719')
+          GROUP BY sales_year, sales_month
+        )
+        WINDOW w AS (
+          ORDER BY sales_year, sales_month
+          ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )
+        LIMIT 1;
+        """,
+        engine=engine
+    )
+    return (temp_trend,)
+
+
+@app.cell
+def _(engine, mo, temp_trend):
+    _df = mo.sql(
+        f"""
+        SELECT slope FROM temp_trend;
+        """,
+        engine=engine
+    )
+    return
+
+
+@app.cell
+def _(Prices, engine, mo, temp_trend):
+    _df = mo.sql(
+        f"""
+        CREATE TEMP TABLE seasonalMeans AS
+        SELECT sales_month, AVG(detrended) AS seasonalMean from
+        (SELECT *, avgPrice + detrend AS detrended FROM
+        (SELECT *,     (julianday(sales_year ||  '-' ||  sales_month || '-01') - julianday('2020-09-01')) * (SELECT slope from temp_trend)   AS detrend FROM
+        (SELECT
+            strftime('%Y', Date)    AS sales_year,
+            strftime('%m', Date)    AS sales_month,
+            AVG(Price)              AS avgPrice
+          FROM Prices
+          WHERE GPU_ID IN ('2364388','2366077','2366719')
+          GROUP BY sales_year, sales_month)
+            )
+        )
+        GROUP BY sales_month
+        """,
+        engine=engine
+    )
+    return (seasonalMeans,)
+
+
+@app.cell
+def _(engine, mo, seasonalMeans):
+    seasonalFactors = mo.sql(
+        f"""
+        SELECT sales_month, seasonalMean / (SELECT sum(seasonalMean) FROM seasonalMeans) AS seasonalFactors FROM seasonalMeans
+        """,
+        engine=engine
+    )
+    return (seasonalFactors,)
+
+
+@app.cell
+def _(seasonalFactors):
+    seasonalFactors.plot(x="sales_month", y="seasonalFactors", drawstyle="steps-post")
+    return
+
+
+@app.cell
+def _():
     return
 
 
